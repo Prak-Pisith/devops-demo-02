595e88646d124c3209fbf6aba8ac36caf98e6b86
prakp <prak.p@netobjex.com>
2023-10-12 10:02:17 +0700

    Implemented docker-compose file

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 3000;
 
 
 app.use('/', (req, res) => {
-  res.send(`Welcome to the Application!!!`);
+  res.send(`
+  <h2>Welcome to the Application</h2>
+  <h3>These API running on Docker containers</h3>
+  <h3>For learning purpose.</h3>
+  <h3>Listening to Runnin-Adam</h3>
+  <h3>Listening to Roar-Katty</h3>
+  `);
 })
 
 
